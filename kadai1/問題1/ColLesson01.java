3199880aef61ebccaaff80eab12b0d1e1ba091c9
ムタイ ヒロスケ <mutaihirosuke@konichiwamutai.local>
2017-09-18 13:53:48 +0900

    課題6

---
+++ kadai1/問題1/ColLesson01.java
@@ -0,0 +1,30 @@
+package kadai1.問題1;
+
+import java.util.Scanner;
+
+public class ColLesson01 {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Word[]words = new Word[10];
+
+		System.out.println("￼英単語と日本語をスペースで区切って入力して下さい。");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		int index = 0;
+		while (!input.equals("e")) {
+						String [] tmp = new String[2];
+						tmp = input.split(" ");
+						Word wd = new Word(tmp[0], tmp[1]);
+						words[index] = wd;
+						index++;
+						System.out.println("次の英単語と日本語を入力して下さい。\"e\"で終了します。");
+						input = sc.nextLine();
+		}
+		for (int i = 0; i < index; i++) {
+			System.out.println("英単語:" + words[i].getEnglish() + "日本語:" + words[i].getJapanese());
+		}
+		System.out.println(index + "件、登録しました。");
+	}
+}
+
+
